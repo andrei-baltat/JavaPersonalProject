2457f6b668e6059a1f21f67661eaf971b129a6bf
baltat.andrei@gmail.com <baltat.andrei@gmail.com>
2019-03-14 20:39:27 +0200

    Added buttons, fields. Need to add containers for each of them

--- src/com/ibm/swingy/MainWindow.java
+++ src/com/ibm/swingy/MainWindow.java
@@ -4,7 +4,6 @@ package com.ibm.swingy;
 
 public class MainWindow {
 	public static void main(String[] args) {
-	      Window nw = new Window("windows", "click", "C:\\linux_stuff\\cucuricu.txt");
-	      nw.writeFileAtButtonPush();
+
 	}
 }
--- src/com/ibm/swingy/Window.java
+++ src/com/ibm/swingy/Window.java
@@ -1,44 +1,49 @@
 package com.ibm.swingy;
-
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 
 public class Window {
-	JFrame newFrame;
-	final JButton button;
-	private String pathToFile;
+	protected static final String testCaseLabel = "Test Case";
+	protected static final String stepsLabel = "Steps";
+	protected static final String actualResultsLabel = "Actual Results";
+	protected static final String statusLabel = "Status";
+	protected static final String commentsLabel = "Comments";
 	
-	Window(String windowName, String buttonName,String pathToFile){
-		this.pathToFile = pathToFile;
-		this.button = new JButton(buttonName);
-		this.newFrame = new JFrame(windowName);
-		this.newFrame.getContentPane().add(button, BorderLayout.CENTER);
-		this.newFrame.pack();
-	    this.newFrame.setVisible(true);
+	
+	JTextField[] textFrames ;
+	JTextArea[] textAreas;
+	JToggleButton passButton;
+	JToggleButton failButton;
+	JButton submit;
+	JFrame frame;
+	
+	
+	
+	Window(int frameDimension) {
+		this.textFrames = new JTextField [3];
+		this.textAreas = new JTextArea[2];
+		
+		this.textFrames[0] = new JTextField("Write here the Test Case title", 40);
+		this.textFrames[1] = new JTextField("Write here the actual result", 40);
+		this.textFrames[2] = new JTextField("Write here Issue number", 40);
+		
+		this.textAreas[0] = new JTextArea("Write here the steps");
+		this.textAreas[1] = new JTextArea("Write here the comments");
+		
+		this.passButton = new JToggleButton("Pass");
+		this.failButton = new JToggleButton("Fail"); 
+		this.submit = new JButton("Submit");
+		
+		
+		this.frame = new JFrame("Test Case Application");
 	}
 	
-	public void writeFileAtButtonPush() {
-		this.button.addActionListener(new ActionListener()
-	      {
-	         public void actionPerformed(ActionEvent e)
-	         {
-					FileWriter pr;
-					try {
-						pr = new FileWriter(pathToFile);
-						pr.write("aaaa");
-						pr.close();
-						newFrame.dispose();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-	         }
-	      });
+	static void showAndPackWindow() {
+		
+		
 	}
+
 }
